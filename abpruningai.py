912e84034ed51405d22893557789b642664ce53e
Giang-Dang <dangvngiang@gmail.com>
2023-03-09 09:00:03 +0700

    another fixing announcements

--- abpruningai.py
+++ abpruningai.py
@@ -20,14 +20,16 @@ class ABPruningAI:
         best move
         :return: The next move to be played.
         """
-        #first move
-        if(self.state.board == game_settings.EMPTY_BOARD or len(self.state.moves) <= 1):
+        # =======================================
+        # FIRST MOVE AND SECOND MOVE
+        if(self.state.board == game_settings.EMPTY_BOARD or len(self.state.moves) <= 3):
             
             # Announcement
             print("AI used random move.")
             return self.random_move(self.state, 1)
 
         # =======================================
+        # CHECKMATE MOVE
         # if opponent or AI has checkmate move, AI will take this move
         # take move if AI has checkmate move
 
@@ -58,41 +60,42 @@ class ABPruningAI:
         print("---------------------------------")
         
         # =======================================
-        # one-end-blocked three and unblocked two combo move (ob3ub2 combo move) 
-        # if opponent or AI has a ob3ub2 combo move, AI will take this move
+        # COMBO MOVE
+        # if opponent or AI has a combo move, AI will take this move
 
         # Announcement
         print("Checking for combo moves...")
 
-        opponent_ob3ub2_move = State.combo_move(self.state.board, game_settings.get_opponent(self.state.current_turn))
-        com_ob3ub2_move = State.combo_move(self.state.board, self.state.current_turn)
+        opponent_combo_move = State.combo_move(self.state.board, game_settings.get_opponent(self.state.current_turn))
+        com_combo_move = State.combo_move(self.state.board, self.state.current_turn)
         
-        if com_ob3ub2_move:
+        if com_combo_move:
             
             # Announcement
-            print("AI has \"one-end-blocked three and unblocked two\" combo move. Take it!")
+            print("AI has a combo move. Take it!")
             
-            return com_ob3ub2_move
+            return com_combo_move
         
-        if opponent_ob3ub2_move: # >=: Prioritize playing the move to the advantage of the player
+        if opponent_combo_move: # >=: Prioritize playing the move to the advantage of the player
             
             # Announcement
-            print("HUMAN has \"one-end-blocked three and unblocked two\" combo move. Block it!")
+            print("HUMAN has a combo move. Block it!")
             
             return com_high_impact_move
 
         # Announcement
-        print("There is no such combo move.")
+        print("There is no combo move.")
         
         # Announcement
         print("---------------------------------")
         
         # =======================================
-        # if opponent or AI has a high impact move, 
+        # HIGH-IMPACT MOVE
+        # if opponent or AI has a high-impact move, 
         # AI will take whether move which has highest score
 
         # Announcement
-        print("Checking for high impact move...")
+        print("Checking for high-impact move...")
 
         if ai_settings.ENABLE_HIGH_IMPACT_MOVE:
             opponent_high_impact_move, opponent_high_impact_score = State.high_impact_move(self.state.board, game_settings.get_opponent(self.state.current_turn))
@@ -100,19 +103,19 @@ class ABPruningAI:
             if opponent_high_impact_move and opponent_high_impact_score > com_high_impact_score:
                 
                 # Announcement
-                print("AI used high impact move function (return a defensive move).")
+                print("AI has discovered that HUMAN has a high-impact move. AI has taken this move (a defensive move.")
                 
                 return opponent_high_impact_move
             
             if com_high_impact_move and com_high_impact_score >= opponent_high_impact_score: # >=: Prioritize playing the move to the advantage of the player
                 
                 # Announcement
-                print("AI used high impact move function (return an offensive move).")
+                print("AI has discovered that it has a high-impact move. AI has taken this move (an offensive move).")
                 
                 return com_high_impact_move
             
             # Announcement
-            print("Ineligible to use high impact move function")
+            print("AI did not discover any high-impact moves.")
         
         # Announcement
         print("---------------------------------")
